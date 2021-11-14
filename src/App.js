873a9fc310f01560b818f90d9fadf1d0fbd234f3
Bruno Mendoza <hola@brunomendoza.es>
2021-11-14 21:01:17 +0000

    Starting to work with react-redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react"
+import React from 'react'
+import { connect } from 'react-redux'
 
-function App() {
+function App () {
   return (
-    <div>App</div>
+    <div>
+      <h1>COUNT GOES HERE</h1>
+      <button>+</button>
+      <button>-</button>
+    </div>
   )
 }
+
 export default App
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById("root"))
+import store from './redux'
+
+import App from './App'
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
